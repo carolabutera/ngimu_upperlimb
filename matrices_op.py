985926a8144ec6a708e9df61578754837b322c24
Carola Butera <carola.butera@mail.polimi.it>
2022-11-08 11:02:07 +0100

    added script for operation with matrices

---
+++ matrices_op.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+from numpy.linalg import norm
+
+def rotX(alpha): 
+    R=np.matrix([[1,0,0],
+                 [0,math.cos(alpha),-math.sin(alpha)],
+                 [0,math.sin(alpha),math.cos(alpha)]])
+    return R
+def rotY(alpha): 
+    R=np.matrix([[math.cos(alpha),0,math.sin(alpha)],
+                 [0,1,0],
+                 [-math.sin(alpha),0,math.cos(alpha)]])
+    return R
+
+def rotZ(alpha): 
+    R=np.matrix([[math.cos(alpha),-math.sin(alpha),0],
+                 [math.sin(alpha),math.cos(alpha),0],
+                 [0,0,1]])
+
+
