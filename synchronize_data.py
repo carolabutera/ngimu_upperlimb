db39324551b2d882eaa742786c3d20e09bba4d32
Carola Butera <carola.butera@mail.polimi.it>
2022-12-05 10:11:26 +0100

    fixed problem of skipped rows in writing csv

--- synchronize_data.py
+++ synchronize_data.py
@@ -13,14 +13,11 @@ PS=[]
 
 
 name_file='synchro_tiago_imu.csv'
-calib_file='calibration_data.csv'
 file=open(name_file,'w',encoding='UTF8')
-file_c=open(calib_file, 'w',encoding='UTF8')
-writer_file=csv.writer(file, delimiter=',')
-writer_calib=csv.writer(file_c, delimiter=',')
+writer_file=csv.writer(file, delimiter=',', lineterminator='\n')
 header=['timestamp','TOxx', 'TOyx','TOzx','TOxy' ,'TOyy', 'TOzy','TOxz' ,'TOyz' ,'TOzz','UAxx', 'UAyx','UAzx','UAxy' ,'UAyy', 'UAzy','UAxz' ,'UAyz' ,'UAzz','FAxx', 'FAyx','FAzx','FAxy' ,'FAyy', 'FAzy','FAxz' ,'FAyz' ,'FAzz','POE','UAE','HR','FE','PS']
 writer_file.writerow(header)
-writer_calib.writerow(header)
+
 
 with open("./221124_NGIMU_TIAGo_data/TIAGo_LogFile_2022_11_24_16_25_36.csv", 'r') as tiago_file:
   csvreader = csv.reader(tiago_file)  
@@ -34,15 +31,12 @@ with open("./221124_NGIMU_TIAGo_data/TIAGo_LogFile_2022_11_24_16_25_36.csv", 'r'
         FE=np.append(FE, float(row[4])*180/3.14)
         PS=np.append(PS, float(row[5])*180/3.14)
 i=0
+print("timestamp length", timestamps.shape)
+
 with open("./221124_NGIMU_TIAGo_data/ROTdata_2022-11-24-16-25-31.csv", 'r') as imu_file:
   csvreader = csv.reader(imu_file) 
   for timestamp in timestamps:
     for row in csvreader:
-        if timestamp==timestamps[0]:
-          if row[0]!='time':
-            if float(row[0])<timestamp:
-              calib_data=[row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21],row[22],row[23],row[24],row[25],row[26],row[27]]
-              writer_calib.writerow(calib_data)
         if row[0]!='time':
             if timestamp<float(row[0]):
                 data=[timestamp,row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21],row[22],row[23],row[24],row[25],row[26],row[27],POE[i],AOE[i],HR[i],FE[i],PS[i]]
